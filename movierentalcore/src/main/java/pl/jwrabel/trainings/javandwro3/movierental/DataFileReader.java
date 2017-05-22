66bf16964145e91cb75cc6cf102594446d0090a9
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-05-22 20:58:13 +0200

    update

--- movierentalcore/src/main/java/pl/jwrabel/trainings/javandwro3/movierental/DataFileReader.java
+++ movierentalcore/src/main/java/pl/jwrabel/trainings/javandwro3/movierental/DataFileReader.java
@@ -1,12 +1,22 @@
 package pl.jwrabel.trainings.javandwro3.movierental;
 
+import java.text.ParseException;
 import java.util.List;
 
 /**
  * Created by jakubwrabel on 22/05/2017.
  */
 public class DataFileReader {
-	public static List<Customer> readCustomersFromFile(String fileName){
+	public static List<Customer> readCustomersFromFile(String fileName) {
+
+		// TODO FIX!!!
+		try {
+			String line = "0,1234,Adam,Miauczyński,Wroclaw,2017-05-22";
+			Customer customer = new Customer(line);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
 		return null;
 	}
 }
