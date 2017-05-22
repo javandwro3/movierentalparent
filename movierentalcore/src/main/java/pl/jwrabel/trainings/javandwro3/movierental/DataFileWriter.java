b0efc7510bb548c02e270b406a6ab5020a09584e
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-05-22 20:54:32 +0200

    update

--- movierentalcore/src/main/java/pl/jwrabel/trainings/javandwro3/movierental/DataFileWriter.java
+++ movierentalcore/src/main/java/pl/jwrabel/trainings/javandwro3/movierental/DataFileWriter.java
@@ -1,5 +1,7 @@
 package pl.jwrabel.trainings.javandwro3.movierental;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -10,13 +12,27 @@ import java.util.List;
 public class DataFileWriter {
 	public static void main(String[] args) {
 		List<Customer> customers = new ArrayList<>();
-		customers.add(new Customer("1234","Adam", "Miauczyński", "Wroclaw", new Date()));
-		customers.add(new Customer("23245","Jan", "Nowak", "Wroclaw", new Date()));
+		customers.add(new Customer("1234", "Adam", "Miauczyński", "Wroclaw", new Date()));
+		customers.add(new Customer("23245", "Jan", "Nowak", "Wroclaw", new Date()));
 
 		writeCustomersToFile("customers.csv", customers);
 	}
 
-	public static void writeCustomersToFile(String fileName, List<Customer> customers){
+	public static void writeCustomersToFile(String fileName, List<Customer> customers) {
+		try {
+			FileWriter fileWriter = new FileWriter(fileName);
+
+			for (Customer customer : customers) {
+				fileWriter.write(customer.toCSVString() + System.lineSeparator());
+				// prawie to samo co:
+//				fileWriter.write(customer.toCSVString() + "\n");
+			}
+
+			fileWriter.flush();
+			fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 }
