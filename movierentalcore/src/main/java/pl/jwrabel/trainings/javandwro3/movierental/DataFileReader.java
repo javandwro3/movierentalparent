ec8584a4ed96989599c51106a3b997c65673548d
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-05-23 18:04:31 +0200

    update

--- movierentalcore/src/main/java/pl/jwrabel/trainings/javandwro3/movierental/DataFileReader.java
+++ movierentalcore/src/main/java/pl/jwrabel/trainings/javandwro3/movierental/DataFileReader.java
@@ -4,7 +4,9 @@ import com.google.common.base.Charsets;
 import com.google.common.io.Files;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,17 +14,23 @@ import java.util.List;
  */
 public class DataFileReader {
 	public static List<Customer> readCustomersFromFile(String fileName) {
+		List<Customer> customers = new ArrayList<>();
 
-		List<String> fileLines = Files.readLines(new File(fileName), Charsets.UTF_8);
-
-		// TODO FIX!!!
 		try {
-			String line = "0,1234,Adam,Miauczyński,Wroclaw,2017-05-22";
-			Customer customer = new Customer(line);
+
+			List<String> fileLines = Files.readLines(new File(fileName), Charsets.UTF_8);
+
+			for (String line : fileLines) {
+				Customer customer = new Customer(line);
+				customers.add(customer);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 
-		return null;
+		return customers;
 	}
 }
